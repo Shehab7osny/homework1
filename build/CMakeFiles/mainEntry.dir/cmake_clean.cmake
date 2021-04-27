b743f4498e5b7f9613f585bf9b51f9b9ace019f0
Federico Corso <federicocorso@MacBook-Pro-di-Federico-2.local>
2021-04-27 16:42:35 +0200

    inclusion of build folder with svg files and executables

---
+++ build/CMakeFiles/mainEntry.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainEntry.dir/main.cpp.o"
+  "CMakeFiles/mainEntry.dir/main.cpp.o.d"
+  "CMakeFiles/mainEntry.dir/src/fc_scara.cpp.o"
+  "CMakeFiles/mainEntry.dir/src/fc_scara.cpp.o.d"
+  "CMakeFiles/mainEntry.dir/src/fc_svg.cpp.o"
+  "CMakeFiles/mainEntry.dir/src/fc_svg.cpp.o.d"
+  "mainEntry"
+  "mainEntry.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainEntry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
